a641ce5401cafed4f9067a42fa7f6533347f6f4e
Gilberto Medina <gilbertomedina0526@icloud.com>
2026-02-11 13:37:17 -0800

    small arithmetic program using Vim

---
+++ vim_practice.cpp
@@ -0,0 +1,26 @@
+// Small C++ program to practice simple arithmetic and print results to the console using Vim.
+
+//Library Headers
+#include <iostream>
+
+//Main entry Point
+
+int main(){
+//Global variables 
+ 
+ int x = 10;
+ int y = -5;
+ std::string name = "Name";
+//Print out the variables
+ std::cout << name << std::endl;// Use endl to automatically flush the output buffer and move to the new line
+ std::cout << x + y << std::endl;
+ std::cout << x - y << std::endl;
+ std::cout << x * y << std::endl;
+ std::cout << x / y << std::endl;
+
+ return 0;
+
+
+
+
+}
